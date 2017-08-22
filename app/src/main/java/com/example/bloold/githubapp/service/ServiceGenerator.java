60a3b7f59f24dcaf71355fb5e608ada665e640a2
Dmitry <donaldjrduck@gmail.com>
2017-08-22 09:49:19 +0200

    Update ServiceGenerator.java
    delete token authorization

--- app/src/main/java/com/example/bloold/githubapp/service/ServiceGenerator.java
+++ app/src/main/java/com/example/bloold/githubapp/service/ServiceGenerator.java
@@ -25,20 +25,9 @@ public class ServiceGenerator {
     static private final String GIT_HUB_API_URL = "https://api.github.com/";
 
     private static Retrofit getRetrofit() {
-
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request request = chain.request();
-                Request authRequest = request.newBuilder().addHeader("Authorization", "Bearer " + PERSONAL_TOKEN).build();
-                return chain.proceed(authRequest);
-            }
-        }).build();
-
         return new Retrofit.Builder()
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .baseUrl(GIT_HUB_API_URL)
-                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
